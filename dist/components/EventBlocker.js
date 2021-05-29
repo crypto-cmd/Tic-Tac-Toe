1c90fc39789843114df7d212267e77febdba3399
Orville Daley <orvilledaley2004@gmail.com>
2021-05-28 23:39:40 -0500

    Updates

--- dist/components/EventBlocker.js
+++
@@ -1,8 +0,0 @@
-import React from "../../_snowpack/pkg/react.js";
-import "./EventBlocker.css.proxy.js";
-const EventBlocker = () => {
-  return /* @__PURE__ */ React.createElement("div", {
-    className: "block"
-  });
-};
-export default EventBlocker;
