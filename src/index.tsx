71c49eae8adc0aa4e5a93aaba8fb9468bd94489c
Orville Daley <orvilledaley2004@gmail.com>
2021-05-28 23:27:32 -0500

    commit on master

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+console.log("index file loaded");
 ReactDOM.render(<App></App>, document.getElementById("root"));
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
-if (import.meta.hot) {
-    import.meta.hot.accept();
-}
+// if (import.meta.hot) {
+//     import.meta.hot.accept();
+// }
