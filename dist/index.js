c504cec72cdd6ff0a43918f14b2549b6082a12c6
Orville Daley <orvilledaley2004@gmail.com>
2021-05-28 23:19:45 -0500

    Updates

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,5 @@
+import React from "../_snowpack/pkg/react.js";
+import ReactDOM from "../_snowpack/pkg/react-dom.js";
+import App from "./App.js";
 console.log("index file loaded");
+ReactDOM.render(/* @__PURE__ */ React.createElement(App, null), document.getElementById("root"));
