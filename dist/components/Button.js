e57e02d6b2923f4ff42c8c30825efccdba9ed964
Orville Daley <orvilledaley2004@gmail.com>
2021-07-03 23:48:25 -0500

    Updates

--- dist/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "../../_snowpack/pkg/react.js";
-import "./Button.css.proxy.js";
-const Button = ({onClick, text}) => {
-  return /* @__PURE__ */ React.createElement("a", {
-    className: "btn",
-    onClick
-  }, text);
-};
-export default Button;
--- dist/components/EventBlocker.js
+++
@@ -1,6 +0,0 @@
-import React from "../../_snowpack/pkg/react.js";
-import "./EventBlocker.css.proxy.js";
-const EventBlocker = () => /* @__PURE__ */ React.createElement("div", {
-  className: "block"
-});
-export default EventBlocker;
