bc414f7257ad25f770cca7722d05e81db34cd15a
Orville Daley <orvilledaley2004@gmail.com>
2021-07-04 00:27:31 -0500

    bug-fix: redeploy gh-pages

--- app/snowpack.config.js
+++ app/snowpack.config.js
@@ -1,14 +1,18 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
-    mount: {
-        // directory name: 'build directory'
-        public: "/",
-    src: "/dist",
-
-    },
-    plugins: ["@snowpack/plugin-react-refresh", "@snowpack/plugin-sass"],
-    buildOptions: {
-        baseUrl: "/Tic-Tac-Toe/",
-        out: "./build",
-    },
+	mount: {
+		// directory name: 'build directory'
+		public: '/',
+		src: '/dist'
+	},
+	plugins: [ , '@snowpack/plugin-react-refresh', '@snowpack/plugin-sass' ],
+	buildOptions: {
+		baseUrl: '/Tic-Tac-Toe/',
+		out: '../build'
+	},
+	optimize: {
+		bundle: true,
+		minify: true,
+		target: 'es2018'
+	}
 };
